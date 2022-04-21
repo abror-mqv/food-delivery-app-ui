606179d0f9dc3edfd02ca0fed3dc11b411f379e8
Abror <mmtklvabrr@gmail.com>
2022-04-22 03:24:04 +0600

    ada

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mamba/constants/colors.dart';
+import 'package:mamba/widgets/custom_app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackground,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(
+            Icons.arrow_back_ios_new_outlined,
+            Icons.search_outlined
+          )
+      ],),
+    );
+  }
+}
